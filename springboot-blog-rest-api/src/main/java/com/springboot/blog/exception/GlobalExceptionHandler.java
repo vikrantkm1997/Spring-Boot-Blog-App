f8cc6c45e816f10de2e82cbfc4c9b6e2d31a9a38
vikrantlearning <vikrantlearning@gmail.com>
2022-04-07 09:33:54 +0530

    Till Customised validation response

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -63,4 +63,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         });
         return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
+
+  /*  @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+                                                                        WebRequest webRequest) {
+        Map<String,String> errors=new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName=((FieldError)error).getField();
+            String message=error.getDefaultMessage();
+            errors.put(fieldName,message);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }*///same as above exception
 }
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,21 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name should not be Null or empty")
     private String name;
+
+    @NotEmpty(message = "Email should not beNull or empty")
+    @Email
     private String email;
+
+    @NotEmpty()
+    @Size(min = 10, message = "Comment body must be of minimum character 10")
     private String body;
 }
